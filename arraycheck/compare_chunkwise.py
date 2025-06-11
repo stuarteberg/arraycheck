80cd9c24b1963a8784f04574571e96915bfd6a00
Stuart Berg <bergs@janelia.hhmi.org>
2025-06-11 16:10:34 -0400

    wip

--- arraycheck/compare_chunkwise.py
+++ arraycheck/compare_chunkwise.py
@@ -1,9 +1,7 @@
 import zarr
 import numpy as np
 import time
-
-from .util import get_chunk_slicing
-
+from typing import Tuple, Iterator
 
 def compare_arrays(
     array1_path: str,
@@ -22,3 +20,27 @@ def compare_arrays(
             return False
 
     return True
+
+
+def get_chunk_slicing(shape: Tuple[int, int, int], chunk_width: int = 256) -> Iterator[Tuple[slice, slice, slice]]:
+    """
+    Generate slicings for iterating over chunks in a 3D array.
+    
+    Args:
+        shape: 3D array shape (Z, Y, X)
+        chunk_width: Size of chunks along each dimension
+        
+    Yields:
+        Slices for (Z, Y, X) dimensions, to be used for extracting chunks from a 3D array.
+    """
+    shape = np.array(shape)
+    grid_shape = (shape + chunk_width - 1) // chunk_width
+    
+    for grid_idx in np.ndindex(*grid_shape):
+        starts = np.array(grid_idx) * chunk_width
+        ends = np.minimum(starts + chunk_width, shape)
+
+        z0, y0, x0 = starts
+        z1, y1, x1 = ends
+
+        yield np.s_[z0:z1, y0:y1, x0:x1]
